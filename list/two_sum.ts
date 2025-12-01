b218be9f9190aaa58f034f2b5f86a194c0ac04e1
vipwhy12 <vipwhy12@naver.com>
2025-12-01 20:15:55 +0900

    fix: two-sum 시간복잡도 개선

--- list/two_sum.ts
+++ list/two_sum.ts
@@ -4,12 +4,16 @@
  */
 
 function twoSum(nums: number[], target: number): number[] {
+  const seen = new Map<number, number>();
+
   for (let i = 0; i < nums.length; i++) {
-    const indexTarget = target - nums[i];
-    const findIndex = nums.indexOf(indexTarget, i + 1);
-    if (findIndex > -1) {
-      return [i, findIndex];
-    }
+    const val = nums[i];
+    const need = target - val;
+
+    if (seen.has(need)) return [seen.get(need)!, i];
+
+    seen.set(val, i);
   }
+
   return [];
 }
